ab2ef17aab1957f79a7602c131267b3672d03087
robert.brosh23 <robert.brosh23@gmail.com>
2024-08-03 15:56:20 -0400

    commit

---
+++ Enemy/enemy.gd
@@ -0,0 +1,72 @@
+extends CharacterBody3D
+class_name Enemy
+
+const SPEED = 3.0
+const JUMP_VELOCITY = 4.5
+
+@export var max_hitpoints := 100
+@export var attack_range := 1.5
+@export var attack_damage := 20
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+@onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var player: CharacterBody3D
+var provoked := false
+var aggro_range := 12.0
+var hitpoints: int = max_hitpoints:
+	set(value):
+		hitpoints = value
+		if hitpoints <= 0:
+			queue_free()
+		provoked = true
+
+
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("player")
+
+
+func _process(delta: float) -> void:
+	if provoked:
+		navigation_agent_3d.target_position = player.global_position
+		if global_position.distance_to(player.global_position) <= attack_range:
+			animation_player.play("Attack")
+
+
+func _physics_process(delta: float) -> void:
+	var next_position = navigation_agent_3d.get_next_path_position()
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	
+	var distance := global_position.distance_to(player.global_position)
+	if distance <= aggro_range:
+		provoked = true
+		
+	# Handle jump.
+	var direction := global_position.direction_to(next_position)
+	if direction:
+		look_at_target(direction)
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	move_and_slide()
+
+
+func look_at_target(direction: Vector3) -> void:
+	var adjusted_direction = direction
+	adjusted_direction.y = 0
+	look_at(global_position + adjusted_direction, Vector3.UP, true)
+	
+
+func attack() -> void:
+	print("enemy attack!")
+	player.hitpoints -= attack_damage
+	
+	
+	
---
+++ Player/player.gd
@@ -0,0 +1,69 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+
+@export var jump_height := 1.0
+@export var fall_multiplier := 2.0
+@export var sensitivity := 0.005
+@export var max_hitpoints := 100
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var mouse_motion := Vector2.ZERO
+var hitpoints: int = max_hitpoints:
+	set(value):
+		hitpoints = value
+		print(hitpoints)
+		if hitpoints <= 0:
+			get_tree().quit()
+		
+@onready var camera_pivot: Node3D = $CameraPivot
+
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta: float) -> void:
+	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		handle_camera_rotation()
+	
+	# Add the gravity.
+	if not is_on_floor():
+		if velocity.y >= 0:
+			velocity.y -= gravity * delta
+		else:
+			velocity.y -= gravity * delta * fall_multiplier
+		
+		
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = sqrt(jump_height * 2.0 * gravity)
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	move_and_slide()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if event is InputEventMouseMotion:
+		mouse_motion = -event.relative * sensitivity
+
+func handle_camera_rotation() -> void:
+	rotate_y(mouse_motion.x)
+	camera_pivot.rotate_x(mouse_motion.y)
+	camera_pivot.rotation_degrees.x = clamp(
+		camera_pivot.rotation_degrees.x, -90.0, 90.0
+	)
+	mouse_motion = Vector2.ZERO
+	

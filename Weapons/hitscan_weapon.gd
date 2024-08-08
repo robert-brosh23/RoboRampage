5851470b7682a40fe17eb9bfa66cae724a91c96a
robert.brosh23 <robert.brosh23@gmail.com>
2024-08-08 18:46:28 -0400

    added ar-15

--- Weapons/hitscan_weapon.gd
+++ Weapons/hitscan_weapon.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 @export var fire_rate := 14.0
 @export var recoil := .05
+@export var damage := 10
+@export var automatic := true
 @export var weapon_mesh : Node3D
 @export var muzzle_flash : GPUParticles3D
 @export var sparks : PackedScene
@@ -16,9 +18,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("fire"):
-		if cooldown_timer.is_stopped():
-			shoot()
+	if automatic:
+		if Input.is_action_pressed("fire"):
+			if cooldown_timer.is_stopped():
+				shoot()
+	else: 
+		if Input.is_action_just_pressed("fire"):
+			if cooldown_timer.is_stopped():
+				shoot()
 			
 	weapon_mesh.position = weapon_mesh.position.lerp(weapon_position, delta * 10.0)
 
@@ -30,7 +37,7 @@ func shoot() -> void:
 	printt("weapon fired!", collider)
 	weapon_mesh.position.z += recoil
 	if collider is Enemy:
-		collider.hitpoints -= 10
+		collider.hitpoints -= damage
 	var spark = sparks.instantiate()
 	add_child(spark)
 	spark.global_position = ray_cast.get_collision_point()
